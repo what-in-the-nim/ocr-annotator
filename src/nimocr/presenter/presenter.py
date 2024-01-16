54bf2567efe1c0675ddeef506f8fe0f1dfe95867
Win <chatcharinsang@gmail.com>
2024-01-16 21:40:47 +0700

    Add comments and renaming functions

--- src/nimocr/presenter/presenter.py
+++ src/nimocr/presenter/presenter.py
@@ -7,6 +7,9 @@ logger = logging.getLogger(__name__)
 
 
 class Presenter(QObject):
+    """
+    This class is used to connect the model and the view.
+    """
     def __init__(self, model: ImageListModel, view: MainWindow):
         super().__init__()
         self.model = model
@@ -31,7 +34,7 @@ class Presenter(QObject):
         """Move the index of the model and update the view"""
         logger.info("Presenter received new index: %s", new_index)
         self.model.move_to(new_index)
-        self.update_widget()
+        self.refresh_widget()
 
     @pyqtSlot(str)
     def fix_label(self, new_text: str) -> None:
@@ -46,21 +49,26 @@ class Presenter(QObject):
     def load_file(self, file_path: str) -> None:
         """Load the file and update the view"""
         logger.info("Presenter received file path: %s", file_path)
+        # Load model from the file path.
         self.model.load_file(file_path)
+        # Get the path column and text column from the user.
         path_column, text_column = self.view.create_select_column_dialog(self.model.columns)
+        # Set the path column and text column to the model.
         self.model.set_path_column_name(path_column)
         self.model.set_text_column_name(text_column)
-        self.view.annotatorWidget.textWidget.initialize_spinbox(
-            self.model.length
-        )
-        self.update_widget()
+        # Initialize the spinbox to match the new length.
+        total_items = self.model.length
+        self.view.annotatorWidget.textWidget.initialize_spinbox(total_items)
+        self.refresh_widget()
 
 
     @pyqtSlot()
     def save_file(self) -> None:
         """Save the file and update the view"""
         logger.info("Presenter received save file request")
+        # Create a save filedialog and get the save path.
         save_path = self.view.create_save_file_dialog(self.model.save_filename)
+        # Save model to the save path.
         self.model.save_file(save_path)
         self.view.show_message(f"File saved at: {save_path}")
 
@@ -68,36 +76,46 @@ class Presenter(QObject):
     def rotate_image(self) -> None:
         """Rotate the image 90 degree and update the view"""
         logger.info("Presenter received rotate image request")
+        # Rotate the current image.
         self.model.rotate_image()
-        self.update_widget()
+        self.refresh_widget()
 
     @pyqtSlot()
     def delete_item(self) -> None:
         """Delete the current item and update the view"""
         logger.info("Presenter received delete row request")
         self.model.delete_item()
-        self.view.annotatorWidget.textWidget.initialize_spinbox(
-            self.model.length
-        )
-        self.update_widget()
+        # Reinitialize the spinbox to match the new length.
+        total_items = self.model.length
+        self.view.annotatorWidget.textWidget.initialize_spinbox(total_items)
+        self.refresh_widget()
 
     @pyqtSlot()
     def backward_image(self) -> None:
+        """Move the index backward and update the view"""
         logger.info("Presenter received backward image request")
+        # Shift the index to the previous item.
         self.model.prev()
-        self.update_widget()
+        self.refresh_widget()
 
     @pyqtSlot()
     def forward_image(self) -> None:
+        """Move the index forward and update the view"""
         logger.info("Presenter received forward image request")
+        # Shift the index to the next item.
         self.model.next()
-        self.update_widget()
+        self.refresh_widget()
 
-    def update_widget(self) -> None:
+    def refresh_widget(self) -> None:
+        """
+        When the model state is updated, 
+        call this function to update the view.
+        """
         image = self.model.image
         # Return if image is exception raised
         if isinstance(image, Exception):
-            self.view.show_message(str(image))
+            error_message = str(image)
+            self.view.show_message(error_message)
         self.view.annotatorWidget.set_image(image)
         self.view.annotatorWidget.set_text(self.model.text)
         self.view.annotatorWidget.set_index(self.model.index)
