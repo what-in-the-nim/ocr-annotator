5626ce5a06ff961e1561bf590cc683a6a1230fcc
Win <chatcharinsang@gmail.com>
2024-01-16 22:05:02 +0700

    Refactor image handling code

--- src/nimocr/model/image_handler.py
+++ src/nimocr/model/image_handler.py
@@ -1,3 +1,5 @@
+import os.path as op
+
 from PIL import Image
 
 
@@ -5,12 +7,12 @@ class ImageHandler:
     @staticmethod
     def open(path: str) -> Image.Image:
         """Return the current image."""
-        try:
-            image = Image.open(path)
-            rgb_image = image.convert("RGB")
-            return rgb_image
-        except FileNotFoundError:
-            raise FileNotFoundError(f"File not found: {path}, Please browse directory to solve this.")
+        if not op.exists(path):
+            return FileNotFoundError(f"File not found: {path}, Please browse directory to solve this.")
+
+        image = Image.open(path)
+        rgb_image = image.convert("RGB")
+        return rgb_image
 
     def rotate(image: Image.Image, degree: int = 90) -> Image.Image:
         """Rotate the current image."""
